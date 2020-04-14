1293d67b2437f12d7d60a2484b67dcd392e56ca6
sonricardo <sonricardo18@gmail.com>
2020-04-14 15:52:04 -0500

    acabe el diseño del splash

--- app/src/main/java/com/example/monopolydm/SplashScreen.kt
+++ app/src/main/java/com/example/monopolydm/SplashScreen.kt
@@ -3,10 +3,10 @@ package com.example.monopolydm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
     }
 }
