524d60ebe0582f899e6355f87b0dda0641dbb657
sonricardo <sonricardo18@gmail.com>
2020-04-18 13:19:16 -0500

    termine el splash

--- app/src/main/java/com/example/monopolydm/SplashScreen.kt
+++ app/src/main/java/com/example/monopolydm/SplashScreen.kt
@@ -1,12 +1,33 @@
 package com.example.monopolydm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.monopolydm.activities.LoginActivity
+
 
 class SplashScreen : AppCompatActivity() {
+
+    val SPLASH_DURATION = 2000L
+    val TAG = "SplashActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+
+        val backgound = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(SPLASH_DURATION)
+                    val intent = Intent(this@SplashScreen, LoginActivity::class.java)
+                    startActivity(intent)
+
+                }catch (e:Exception){
+                    Log.d(TAG,e.message!!)
+                }
+            }
+        }
+        backgound.start()
     }
 }
---
+++ app/src/main/java/com/example/monopolydm/activities/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.monopolydm.activities
+
+//import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.monopolydm.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
